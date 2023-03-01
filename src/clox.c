d2b63462e1520b753616f1f371323174e4fdf5fa
bcarlborg <bcarlborg@gmail.com>
2023-03-01 12:25:26 -0800

    using the value stack within the vm to push and pop constants -- finished with 15.2

--- src/clox.c
+++ src/clox.c
@@ -1,19 +1,28 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
+  int constant_index;
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
 
-  int constant_index = addConstant(&chunk, 1.2);
+  constant_index = addConstant(&chunk, 99.99);
+  writeChunk(&chunk, OP_CONSTANT, 122);
+  writeChunk(&chunk, constant_index, 123);
+
+  constant_index = addConstant(&chunk, 1.2);
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant_index, 123);
 
   writeChunk(&chunk, OP_RETURN, 123);
 
-  disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
 
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
