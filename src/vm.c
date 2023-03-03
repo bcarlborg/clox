bc2a8e26b80fac984b498e4e9e1d9ed9bd89916e
bcarlborg <bcarlborg@gmail.com>
2023-03-02 16:29:20 -0800

    finished chapter 17

--- src/vm.c
+++ src/vm.c
@@ -80,7 +80,19 @@ static InterpretResult run() {
 }
 
 InterpretResult interpret(const char *source) {
-  compile(source);
+  Chunk chunk;
+  initChunk(&chunk);
+
+  if (!compile(source, &chunk)) {
+    freeChunk(&chunk);
+    return INTERPRET_COMPILE_TIME_ERROR;
+  }
+
+  vm.chunk = &chunk;
+  vm.ip = vm.chunk->code;
+
+  InterpretResult result = run();
+
   return INTERPRET_OK;
 }
 
